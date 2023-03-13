4ba196004b6c1f02174809e6ccec123d8e907a26
Jeong Yoon <dlwjddbs14@naver.com>
2023-03-13 16:24:23 +0900

    feat: user router

--- src/main/kotlin/oecy/jp/kr/oecybackend/presentation/router/RouterConfig.kt
+++ src/main/kotlin/oecy/jp/kr/oecybackend/presentation/router/RouterConfig.kt
@@ -1,19 +1,27 @@
 package oecy.jp.kr.oecybackend.presentation.router
 
 import oecy.jp.kr.oecybackend.presentation.handler.HealthCheckHandler
+import oecy.jp.kr.oecybackend.presentation.handler.UserHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.server.coRouter
 
 @Configuration
-class RouterConfig {
+class RouterConfig(
+    private val healthCheckHandler: HealthCheckHandler,
+    private val userHandler: UserHandler
+) {
 
     @Bean
-    fun apiRouter(healthCheckHandler: HealthCheckHandler) = coRouter {
+    fun apiRouter() = coRouter {
         "/".nest {
             contentType(MediaType.APPLICATION_JSON)
             GET("/", healthCheckHandler::healthCheck)
+        }
+        "/api/v1/users".nest {
+            contentType(MediaType.APPLICATION_JSON)
+            POST("/signup", userHandler::signUp)
         }
     }
 }
